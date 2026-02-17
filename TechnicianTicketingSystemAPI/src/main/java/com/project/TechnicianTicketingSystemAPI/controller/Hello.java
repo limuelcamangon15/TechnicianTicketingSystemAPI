82e19a18f0baca3949bc6a303d98a83c316b2c48
Limuel <limuelcamangon15>
2026-02-17 23:42:30 +0800

    update: structured the folders

--- TechnicianTicketingSystemAPI/src/main/java/com/project/TechnicianTicketingSystemAPI/controller/Hello.java
+++ TechnicianTicketingSystemAPI/src/main/java/com/project/TechnicianTicketingSystemAPI/controller/Hello.java
@@ -1,4 +1,4 @@
-package com.project.TechnicianTicketingSystemAPI.Controller;
+package com.project.TechnicianTicketingSystemAPI.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
