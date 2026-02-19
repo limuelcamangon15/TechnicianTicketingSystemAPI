da707e3d3120462d7535c3b7f16582625214a98b
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-02-19 23:56:47 +0800

    update: added initial service for technician entity

--- TechnicianTicketingSystemAPI/src/main/java/com/project/TechnicianTicketingSystemAPI/service/TechnicianService.java
+++ TechnicianTicketingSystemAPI/src/main/java/com/project/TechnicianTicketingSystemAPI/service/TechnicianService.java
@@ -1,7 +1,11 @@
 package com.project.TechnicianTicketingSystemAPI.service;
 
+import com.project.TechnicianTicketingSystemAPI.model.Technician;
 import com.project.TechnicianTicketingSystemAPI.repository.TechnicianRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TechnicianService {
@@ -11,4 +15,17 @@ public class TechnicianService {
         this.technicianRepository = technicianRepository;
     }
 
+    // CREATE
+    public Technician createTechnician(Technician technician){
+        return technicianRepository.save(technician);
+    }
+    
+    // READ
+    public Optional<Technician> getTechnicianById(Long id){
+        return technicianRepository.findById(id);
+    }
+
+    public List<Technician> getAllTechnicians(){
+        return technicianRepository.findAll();
+    }
 }
