5f1282fa9b8e44c6a33f3cf54cf4390194efc063
limuelcamangon15 <limuelmartincamangon@gmail.com>
2026-02-22 13:33:02 +0800

    update: technician controller done

--- TechnicianTicketingSystemAPI/src/main/java/com/project/TechnicianTicketingSystemAPI/controller/TechnicianController.java
+++ TechnicianTicketingSystemAPI/src/main/java/com/project/TechnicianTicketingSystemAPI/controller/TechnicianController.java
@@ -1,8 +1,11 @@
 package com.project.TechnicianTicketingSystemAPI.controller;
 
+import com.project.TechnicianTicketingSystemAPI.model.Technician;
 import com.project.TechnicianTicketingSystemAPI.service.TechnicianService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/technician")
@@ -12,5 +15,37 @@ public class TechnicianController {
 
     public TechnicianController(TechnicianService technicianService){
         this.technicianService = technicianService;
+    }
+
+    // POST
+    @PostMapping
+    public Technician createTechnician(@RequestBody Technician technician){
+        return technicianService.createTechnician(technician);
+    }
+
+    // GET
+    @GetMapping
+    public List<Technician> getAllTechnicians(){
+        return technicianService.getAllTechnicians();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Technician> getTechnicianById(@PathVariable Long id){
+        return technicianService.getTechnicianById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // PUT
+    @PutMapping("/{id}")
+    public Technician updateTechnician(@PathVariable Long id, @RequestBody Technician technician){
+        technician.setTechnicianId(id);
+        return technicianService.updateTechnician(technician);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public void deleteTechnician(@PathVariable Long id){
+        technicianService.deleteTechnicianById(id);
     }
 }
